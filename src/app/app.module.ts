b889110786b401fd4b672ae9f52b9fa41119fc8f
Hadjadj Sofiane <sofiane.hadjadj.etu@unilim.fr>
2017-12-09 13:07:36 +0100

    2. Correction bug carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    CarouselComponent
+    CarouselComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
