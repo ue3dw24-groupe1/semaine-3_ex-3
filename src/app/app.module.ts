952ee6b00ea57d9d97f0ea0889efaf822bab9e29
Hadjadj Sofiane <sofiane.hadjadj.etu@unilim.fr>
2017-12-09 12:39:04 +0100

    1. Initialisation du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
